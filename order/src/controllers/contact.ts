656d9bbcc466b99009f74f457b8bfe19e1526bc7
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-09-24 22:50:41 +0200

    add the order service

---
+++ order/src/controllers/contact.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+
+/**
+ * GET /contact
+ * Contact form page.
+ */
+export let getContact = (req: Request, res: Response) => {
+    res.json([{name: "hello world", type: "waow", id: 1}]);
+};
+
+/**
+ * POST /contact
+ * Send a contact form via Nodemailer.
+ */
+export let postContact = (req: Request, res: Response) => {
+    req.assert("name", "Name cannot be blank").notEmpty();
+    req.assert("message", "Message cannot be blank").notEmpty();
+
+    const errors = req.validationErrors();
+
+    if (errors) {
+        return res.redirect("/contact");
+    }
+
+    const mailOptions = {
+        to: "your@email.com",
+        from: `${req.body.name} <${req.body.email}>`,
+        subject: "Contact Form",
+        text: req.body.message
+    };
+};
