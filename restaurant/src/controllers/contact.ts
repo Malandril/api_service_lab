baeb9e2c3f230d31e812c9832a81837d7891c47d
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-09-24 17:13:43 +0200

    Initial commit

---
+++ restaurant/src/controllers/contact.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+
+/**
+ * GET /contact
+ * Contact form page.
+ */
+export let getContact = (req: Request, res: Response) => {
+  res.render("contact", {
+    title: "Contact"
+  });
+};
+
+/**
+ * POST /contact
+ * Send a contact form via Nodemailer.
+ */
+export let postContact = (req: Request, res: Response) => {
+  req.assert("name", "Name cannot be blank").notEmpty();
+  req.assert("email", "Email is not valid").isEmail();
+  req.assert("message", "Message cannot be blank").notEmpty();
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    return res.redirect("/contact");
+  }
+
+  const mailOptions = {
+    to: "your@email.com",
+    from: `${req.body.name} <${req.body.email}>`,
+    subject: "Contact Form",
+    text: req.body.message
+  };
+};
