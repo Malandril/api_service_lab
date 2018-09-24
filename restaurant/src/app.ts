f6a9b70c02c9bfd08bb2c860a708e8b056375525
Thomas Canava <thomas.canava@etu.unice.fr>
2018-09-24 20:59:00 +0200

    fixing some version problems

--- restaurant/src/app.ts
+++ restaurant/src/app.ts
@@ -2,12 +2,9 @@ import express from "express";
 import compression from "compression";  // compresses requests
 import session from "express-session";
 import bodyParser from "body-parser";
-import mongo from "connect-mongo";
 import path from "path";
 import mongoose from "mongoose";
 import expressValidator from "express-validator";
-
-const MongoStore = mongo(session);
 
 
 // Controllers (route handlers)
@@ -22,8 +19,9 @@ const app = express();
 
 // Connect to MongoDB
 const mongoUrl = MONGODB_URI;
-mongoose.connect(mongoUrl, {useMongoClient: true}).then(
-  () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+mongoose.connect(mongoUrl, {useNewUrlParser: true}).then(
+    () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
+    },
 ).catch(err => {
   console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
   // process.exit();
