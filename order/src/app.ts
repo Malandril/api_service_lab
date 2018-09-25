43c9625f6b133ce00f62baa2e086f099a607e900
Thomas Canava <thomas.canava@etu.unice.fr>
2018-09-25 23:51:55 +0200

    first docker compose

--- order/src/app.ts
+++ order/src/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import compression from "compression";  // compresses requests
-import session from "express-session";
 import bodyParser from "body-parser";
 import path from "path";
 import mongoose from "mongoose";
