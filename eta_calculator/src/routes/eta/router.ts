8d240ee356ad117c32bb826535aeb5e1807780b1
Loic Gardaire <loic.gardaire@etu.unice.fr>
2018-09-27 11:04:24 +0200

    first version of ETA calculator (not compiling)

---
+++ eta_calculator/src/routes/eta/router.ts
@@ -0,0 +1,18 @@
+import { Request, Response, Router } from "express";
+import {PreparationTimeMealModel} from "../../models";
+
+const router = Router();
+
+const data = [new PreparationTimeMealModel({"name": "Pizza", "price": 8}, 42),
+    new PreparationTimeMealModel({"name": "Pasta", "price": 3}, 24)];
+
+/**
+ * GET /eta/:mealId
+ * Return the ETA of the specified meal
+ */
+const getETAForMeal = (req: Request, res: Response) => {
+    res.json(data[req.params.mealId].time_to_prepare);
+};
+router.get("/:mealId", getETAForMeal);
+
+export default router;
