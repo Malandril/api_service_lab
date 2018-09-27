1a3b1d9cfaf5fbb9ac4ae6f8fb1c2a9647687625
Loic Gardaire <loic.gardaire@etu.unice.fr>
2018-09-27 13:50:17 +0200

    ETA calculator simplified : no model needed

--- eta_calculator/src/routes/eta/router.ts
+++ eta_calculator/src/routes/eta/router.ts
@@ -1,10 +1,8 @@
 import { Request, Response, Router } from "express";
-import { PreparationTimeMealModel } from "../../models";
 
 const router = Router();
 
-const data = [new PreparationTimeMealModel({"name": "Pizza", "price": 8}, 42),
-    new PreparationTimeMealModel({"name": "Pasta", "price": 3}, 24)];
+const data = [42, 24];
 
 
 /**
@@ -12,7 +10,7 @@ const data = [new PreparationTimeMealModel({"name": "Pizza", "price": 8}, 42),
  * Return the ETA for the specified meal
  */
 const getETAForMeal = (req: Request, res: Response) => {
-    res.json(data[req.params.mealId].time_to_prepare);
+    res.json(data[req.params.mealId]);
 };
 router.get("/:mealId", getETAForMeal);
 
