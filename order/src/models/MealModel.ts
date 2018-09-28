4c31f223bd9a9f2f3e2d04995ce29725c4da9492
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-09-28 19:09:55 +0200

    add put to orders and create all routes for orders

--- order/src/models/MealModel.ts
+++ order/src/models/MealModel.ts
@@ -3,9 +3,11 @@ import { Meal } from "../../../commons/models";
 export class MealModel implements Meal {
     name: string;
     price: number;
+    id: number;
 
     constructor(meal: Meal) {
         this.name = meal.name;
         this.price = meal.price;
+        this.id = meal.id;
     }
 }
