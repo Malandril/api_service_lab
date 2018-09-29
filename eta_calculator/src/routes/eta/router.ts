ac1b035354c5ff21a5a94e76319b14475fa79756
Loic Gardaire <loic.gardaire@etu.unice.fr>
2018-09-29 12:11:59 +0200

    ETA calculator calculate total ETA for multi meals with POST

--- eta_calculator/src/routes/eta/router.ts
+++ eta_calculator/src/routes/eta/router.ts
@@ -1,48 +1,32 @@
 import { Request, Response, Router } from "express";
-import {MealsListModel} from "../../models/MealsListModel";
 
 const {check, validationResult} = require("express-validator/check");
 const router = Router();
 
-const data = [42, 24];
-
-
-/**
- * GET /eta/:mealId
- * Return the ETA for the specified meal
- */
-const getETAForMeal = (req: Request, res: Response) => {
-    res.json(data[req.params.mealId]);
-};
-router.get("/:mealId", getETAForMeal);
-
 
 /**
  * POST /eta
- * Create the specified meal
+ * Calculate the total ETA for the specified meals
  */
 const postEta = (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(422).json({errors: errors.array()});
     }
-    const meals = new MealsListModel(
-        {"client": 2,
-            "meals": [
-                {"name": "Lol", "price": 12}, {"name": "Lol", "price": 12}
-            ]
-        });
-    // const o = new MealModel({"name": req.body.name, "price": +req.body.price, "id": nextId});
-    // data[nextId++] = o;
-    // res.status(201).json(o);
+    let totalETA = 0;
+    for (let i = 0; i < req.body.meals.length; i++) {
+        totalETA += req.body.meals[i].eta;
+    }
+    res.status(200).json(totalETA);
 };
 
-function getMealValidator() {
+function getETAValidator() {
     return [
-        check("name").isString().isLength({min: 1}).withMessage("A meal needs a name"),
-        check("price").isFloat({gt: 0}).withMessage("A meal needs a price greater than 0")
+        check("meals").isArray().isLength({min: 1}).withMessage("The request must contains at least one meal")
     ];
 }
+
+router.post("/", getETAValidator(), postEta);
 
 
 export default router;
