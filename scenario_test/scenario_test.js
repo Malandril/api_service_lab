a8eae6cc06014503efc10d73c4de8d9ac888ad2c
Thomas Canava <canava.thomas@gmail.com>
2018-10-03 00:08:38 +0200

    Optimized dockerfile, continued the client, added simple category sorting to order/meals, and fixed package.json eta_calculator

--- scenario_test/scenario_test.js
+++ scenario_test/scenario_test.js
@@ -1,22 +1,33 @@
 const request = require("request-promise");
 const assert = require("assert");
 
-let order_url = "http://localhost:3000";
+let order_url = "http://localhost:8000";
+let eta_url = "http://localhost:8070";
 
 
 // getting all meals
-request(`${order_url}/meals`, null, function (error, response, body) {
+request({url: `${order_url}/meals`, qs: {category: "Asian"}}, function (error, response, body) {
     assert(response.statusCode, 200);
-    console.log(body);
-    return body[0]
-}).then(function (meal) {
-   return request.post(`${order_url}/orders`, {client: 23, meal: [meal]}, function (error, response, body) {
+    console.log("meals", body);
+}).then(function (meals) {
+    let order = {client: 23, meals: JSON.parse(meals)};
+    return request.post({
+            url: `${order_url}/orders`,
+            form: order
+        }, function (error, response, body) {
             assert(response.statusCode, 201);
-            console.log(body);
+            console.log("ordered", body);
         }
     );
 }).then(function (order) {
-    console.log(order);
+    console.log("order", {order: JSON.parse(order)});
+    return request.post({
+        url: `${eta_url}/eta`,
+        form: {order: JSON.parse(order)}
+    }, function (error, response, body) {
+        assert(response.statusCode, 200);
+        console.log("eta", body)
+    });
 
 });
 
