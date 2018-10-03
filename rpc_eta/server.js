39e4233c1bc6d3539a0d435bf108063dd78bb089
Loic Gardaire <loic.gardaire@etu.unice.fr>
2018-10-03 18:05:15 +0200

    clean up

--- rpc_eta/server.js
+++ rpc_eta/server.js
@@ -12,7 +12,7 @@ let routes = {
     '/eta': function (body) {
         return new Promise((resolve, reject) => {
             if (!body) {
-                throw new (`RPC request was expecting some data...!`);
+                throw new (`RPC request was expecting some data`);
             }
             let _json = JSON.parse(body); // might throw error
             let keys = Object.keys(_json);
@@ -46,47 +46,14 @@ let routes = {
             });
         });
     }
-
-    // // this is our docs endpoint
-    // // through this the clients should know
-    // // what methods and datatypes are available
-    // '/describe': function () {
-    //     // load the type descriptions
-    //     return new Promise(resolve => {
-    //         let type = {};
-    //         let method = {};
-    //
-    //         // set types
-    //         type = types;
-    //
-    //         //set methods
-    //         for(let m in methods) {
-    //             let _m = JSON.parse(JSON.stringify(methods[m]));
-    //             method[m] = _m;
-    //         }
-    //
-    //         resolve({
-    //             types: type,
-    //             methods: method
-    //         });
-    //     });
-    // }
 };
 
-// request Listener
-// this is what we'll feed into http.createServer
 function requestListener(request, response) {
     let reqUrl = `http://${request.headers.host}${request.url}`;
     let parseUrl = url.parse(reqUrl, true);
     let pathname = parseUrl.pathname;
-
-    // we're doing everything json
     response.setHeader('Content-Type', 'application/json');
-
-    // buffer for incoming data
     let buf = null;
-
-    // listen for incoming data
     request.on('data', data => {
         if (buf === null) {
             buf = data;
@@ -94,8 +61,6 @@ function requestListener(request, response) {
             buf = buf + data;
         }
     });
-
-    // on end proceed with compute
     request.on('end', () => {
         let body = buf !== null ? buf.toString() : null;
 
@@ -103,28 +68,24 @@ function requestListener(request, response) {
             let compute = routes[pathname].call(null, body);
 
             if (!(compute instanceof Promise)) {
-                // we're kinda expecting compute to be a promise
-                // so if it isn't, just avoid it
-
                 response.statusCode = 500;
-                response.end('oops! server error!');
-                console.warn(`whatever I got from rpc wasn't a Promise!`);
+                response.end('Server error');
+                console.warn(`Not a Promise`);
             } else {
                 compute.then(res => {
                     response.end(JSON.stringify(res))
                 }).catch(err => {
                     console.error(err);
                     response.statusCode = 500;
-                    response.end('oops! server error!');
+                    response.end('Server error');
                 });
             }
 
         } else {
             response.statusCode = 404;
-            response.end(`oops! ${pathname} not found here`)
+            response.end(`${pathname} not found here`)
         }
     })
 }
 
-// now we can start up the server
 server.listen(PORT);
