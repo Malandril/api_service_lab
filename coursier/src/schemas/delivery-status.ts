93cfce2aecd9c0019ad2046cd80e01f62ab44437
arsebac <francois.melkonian@gmail.com>
2018-10-04 10:47:51 +0200

    implement persistance in coursier

---
+++ coursier/src/schemas/delivery-status.ts
@@ -0,0 +1,22 @@
+import {Document, Schema, Model, model} from "mongoose";
+import {IDeliveryStatus} from "../models/dao/IDeliveryStatus";
+
+export interface IDeliveryStatusModel extends IDeliveryStatus, Document {
+    recordEventToHistory(event: string): void;
+}
+
+export let DeliveryStatusSchema: Schema = new Schema({
+    idDelivery: Number,
+    creation: Date,
+    status: String,
+    history: [{status: String, event: String}]
+});
+DeliveryStatusSchema.pre("save", function (next) {
+    console.log("Save " + this);
+    next();
+});
+
+DeliveryStatusSchema.methods.recordEventToHistory = function (event: string): void {
+    this.history.push({status: this.status, event: event});
+};
+export const IDeliveryStatusModel: Model<IDeliveryStatusModel> = model<IDeliveryStatusModel>("DeliveryStatus", DeliveryStatusSchema);
