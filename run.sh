1f9faaf98a1de8c1b0b0586b5dbda060c13c5ec3
Thomas Canava <canava.thomas@gmail.com>
2018-10-07 17:22:12 +0200

    fixed run and install sh to be compliant with the delivery specifications

--- run.sh
+++ run.sh
@@ -1,32 +1,9 @@
 #!/usr/bin/env bash
-
-echo "You need docker and docker-compose to run the test scenario"
-docker-compose up -d
-arr=( "http://localhost:8000/meals" )
-for url in "${arr[@]}";do
-    declare status
-    for retry in 1 2 3 4 5 6;do
-        status=$(curl -s --head ${url} | head -1 | wc -l)
-        if [ $status -eq 1 ];then
-            break
-        fi
-        sleep 2
-    done
-    if [ $status -eq 1 ];then
-        echo ${url} ${status}
-    else
-        echo "Failed can't access" ${url}
-        docker-compose down
-        exit 2
-    fi
-done
 cd scenario_test
 npm start
 if [ $? -ne 0 ]
 then
     echo "Test failed"
-    docker-compose down
     exit 1
 fi
-docker-compose down
 echo "Test succeeded"
