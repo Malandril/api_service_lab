831bbc4e7016a220791e6da80a3c3950488665eb
Thomas Canava <canava.thomas@gmail.com>
2018-10-18 15:43:06 +0200

    first version of payment

---
+++ payment/src/mongo-helper.js
@@ -0,0 +1,23 @@
+
+const MongoClient = require('mongodb').MongoClient;
+
+module.exports = {
+    client: null,
+    db: null,
+    initialize: function (obj) {
+        obj.client = new MongoClient("mongodb://localhost:27019/" , { useNewUrlParser: true });
+        obj.client.connect(function(err) {
+            console.log("Connected successfully to server");
+            console.log(err);
+            obj.db = obj.client.db("payment");
+
+
+            obj.db.createCollection("payments", { "capped": true, "size": 100000, "max": 5000},
+                function(err, results) {
+                    console.log("Collection created." + err + results);
+                    //client.close();
+                }
+            );
+        });
+    }
+}
