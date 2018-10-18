9e80b87c14d069d96c2b7fe826b192ccf836fbc7
Loic Gardaire <loic.gardaire@gmail.com>
2018-10-18 11:29:42 +0200

    kafka for ETA

--- eta_calculator/src/methods.js
+++ eta_calculator/src/methods.js
@@ -1,28 +1,26 @@
 'use strict';
 
 let methods = {
-    calculateETA: {
-        description: `calculates the ETA for the order, and returns it`,
-        params: ['order:the order object'],
-        returns: ['order'],
-        exec(orderObj) {
-            return new Promise((resolve) => {
-                if (typeof (orderObj) !== 'object') {
-                    throw new Error('An object was expected');
+    calculateETA: function (msg, producer) {
+        let _orderObj = JSON.parse(msg);
+        if (_orderObj.meals == null) {
+            console.error('Your order must contain a list of meals.');
+        } else {
+            let totalETA = 0;
+            for (let i = 0; i < _orderObj.meals.length; i++) {
+                if (_orderObj.meals[i].eta == null) {
+                    console.error('This meal does not have an ETA : ' + JSON.stringify(_orderObj.meals[i]));
+                } else {
+                    totalETA += _orderObj.meals[i].eta
                 }
-                let _orderObj = JSON.parse(JSON.stringify(orderObj));
-                if (_orderObj.meals == null){
-                    resolve('Your order must contain a list of meals.');
-                }
-                let totalETA = 0;
-                for (let i = 0; i < _orderObj.meals.length; i++) {
-                    if(_orderObj.meals[i].eta == null){
-                        resolve('This meal does not have an ETA : '+JSON.stringify(_orderObj.meals[i]));
-                    } else {
-                        totalETA += _orderObj.meals[i].eta
-                    }
-                }
-                resolve(totalETA);
+            }
+            let response = {
+                eta:  totalETA
+            };
+            console.log("Send event eta_result : " + JSON.stringify(response));
+            producer.send({
+                topic:"eta_result",
+                messages: [{key:"", value: response}]
             });
         }
     }
