e4935131bd05ce5581d2a7ee720572add66e510a
Thomas Canava <canava.thomas@gmail.com>
2018-10-18 20:39:52 +0200

    Database retry

--- payment/src/mongo-helper.js
+++ payment/src/mongo-helper.js
@@ -1,23 +1,34 @@
-
 const MongoClient = require('mongodb').MongoClient;
 
+const MAX_RETRY = 5;
 module.exports = {
     client: null,
     db: null,
     initialize: function (obj) {
-        obj.client = new MongoClient("mongodb://mongo_payment:27019/" , { useNewUrlParser: true });
-        obj.client.connect(function(err) {
-            console.log("Connected successfully to server");
-            console.log(err);
-            obj.db = obj.client.db("payment");
-
-
-            obj.db.createCollection("payments", { "capped": true, "size": 100000, "max": 5000},
-                function(err, results) {
-                    console.log("Collection created." + err + results);
-                    //client.close();
+        obj.client = new MongoClient("mongodb://mongo_payment:27019/", {useNewUrlParser: true});
+        let count = 0;
+        let connectWithRetry = function () {
+            obj.client.connect(function (err) {
+                console.log("Connected successfully to server");
+                if (err) {
+                    if (count === MAX_RETRY) {
+                        console.error("Couldn't connect to mongo database");
+                        throw err;
+                    }
+                    console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
+                    count++;
+                    setTimeout(connectWithRetry, 5000);
+                } else {
+                    obj.db = obj.client.db("payment");
+                    obj.db.createCollection("payments", {"capped": true, "size": 100000, "max": 5000},
+                        function (err, results) {
+                            console.log("Collection created." + err + results);
+                            //client.close();
+                        }
+                    );
                 }
-            );
-        });
+            });
+        };
+        connectWithRetry()
     }
-}
+};
