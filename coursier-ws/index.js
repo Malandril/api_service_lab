4112c0d9d4576385f34661039fc04732afc39dc1
François Melkonian <francois.melkonian@gmail.com>
2018-10-20 00:34:40 +0200

    Create coursier gateway

---
+++ coursier-ws/index.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const util = require('util');
+const app = express();
+const port = 3000;
+const {Kafka, logLevel} = require('kafkajs');
+
+var Queue = require('queue-fifo');
+var queue = new Queue();
+
+const kafka = new Kafka({
+    logLevel: logLevel.INFO,
+    brokers: ["kafka:9092"],
+    connectionTimeout: 3000,
+    clientId: 'coursierws',
+});
+
+const log = true;
+
+const listResponse = kafka.consumer({groupId: 'list_orders_to_be_delivered'});
+const producer = kafka.producer();
+
+const run = async () => {
+    await producer.connect();
+    await listResponse.connect();
+    await listResponse.subscribe({topic: "list_orders_to_be_delivered"});
+
+    await listResponse.run({
+        eachMessage: async ({topic, partition, message}) => {
+            console.log("eachMessage " + topic+ " " + message);
+            if (!queue.isEmpty()) {
+                queue.dequeue()(message);
+            } else {
+                console.log("Unable to process list_orders_to_be_delivered response: " + util.inspect(message))
+            }
+        }
+    });
+
+};
+
+run().catch(e => console.error(`[example/consumer] ${e.message}`, e));
+
+const errorTypes = ['unhandledRejection', 'uncaughtException'];
+const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2'];
+
+errorTypes.map(type => {
+    process.on(type, async e => {
+        try {
+            console.log(`process.on ${type}`);
+            console.error(e);
+            await listResponse.disconnect();
+            process.exit(0)
+        } catch (_) {
+            process.exit(1)
+        }
+    })
+});
+signalTraps.map(type => {
+    process.once(type, async () => {
+        try {
+            await listResponse.disconnect();
+            await producer.disconnect();
+        } finally {
+            process.kill(process.pid, type)
+        }
+    })
+});
+
+app.get('/deliveries/', (req, res) => {
+    console.log("Received : " + util.inspect(req.query));
+    if (!("id" in req.query)) {
+            res.send("Attribute 'id' needed");
+            return;
+    }
+    const coursierId = req.query.id;
+    if (!("address" in req.query)) {
+            res.send("Attribute 'address' needed");
+            return;
+    }
+    const address = req.query.address;
+    console.log("Parsed : id=" + coursierId + ", address= " + address);
+
+    let value = JSON.stringify({
+        coursier: {
+            id: coursierId,
+            address: address
+        }
+    });
+    console.log("Send : " + util.inspect(value));
+    producer.send({
+        topic: "get_ordered_to_be_delivered",
+        messages: [{
+            key: "", value: value
+        }]
+    });
+    queue.enqueue(function (msg) {
+        console.log("unqueue : " + msg.value);
+        res.send(msg.value.toString());
+    })
+
+
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
