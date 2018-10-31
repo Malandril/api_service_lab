27ea34fa8c892f5cdd9d812f44b59e7dc0c56338
Thomas Canava <canava.thomas@gmail.com>
2018-10-31 23:10:52 +0100

    Trying first version of service test, (buggy on windows)

--- payment/src/mongo-helper.js
+++ payment/src/mongo-helper.js
@@ -1,17 +1,18 @@
 const MongoClient = require('mongodb').MongoClient;
+const config = require('./configuration.js');
 
 const MAX_RETRY = 5;
 module.exports = {
     client: null,
     db: null,
     initialize: function (obj) {
-        obj.client = new MongoClient("mongodb://mongo_payment:27017/", {useNewUrlParser: true});
+        obj.client = new MongoClient(config.MONGO_URL, {useNewUrlParser: true});
         let count = 0;
         let connectWithRetry = function () {
             obj.client.connect(function (err) {
                 if (err) {
                     if (count === MAX_RETRY) {
-                        console.error("Couldn't connect to mongo database",err);
+                        console.error("Couldn't connect to mongo database", err);
                         throw err;
                     }
                     console.error('Failed to connect to mongo on startup - retrying in 5 sec');
@@ -23,7 +24,8 @@ module.exports = {
                     obj.db = obj.client.db("payment");
                     obj.db.createCollection("payments", {"capped": true, "size": 100000, "max": 5000},
                         function (err, results) {
-                            console.log("Collection created." + err + results);
+                        if(!err)
+                            console.log("Payment collection created.");
                             //client.close();
                         }
                     );
--- payment/test/test_kafka.js
+++ payment/test/test_kafka.js
@@ -1,11 +1,8 @@
 'use strict';
 
-let methods = require('./methods');
-let config = require('./configuration');
+let config = require('../src/configuration');
 const {Kafka, logLevel} = require('kafkajs');
-let mongoHelper = require("./mongo-helper");
 
-mongoHelper.initialize(mongoHelper);
 
 const kafka = new Kafka({
     logLevel: logLevel.NOTHING,
@@ -13,21 +10,40 @@ const kafka = new Kafka({
     connectionTimeout: 3000,
     clientId: 'delivery_man_account',
 });
-const submitOrder = kafka.consumer({groupId: 'payment'});
-const producer = kafka.producer();
 
+const submitOrder = kafka.consumer({groupId: 'payment_test'});
+const producer = kafka.producer();
+console.log("started test");
 const run = async () => {
     await producer.connect();
     await submitOrder.connect();
-    await submitOrder.subscribe({topic: "submit_order"});
+    await submitOrder.subscribe({topic: "payment_succeeded"});
+    await submitOrder.subscribe({topic: "payment_failed"});
     await submitOrder.run({
         eachMessage: async ({topic, partition, message}) => {
-            methods.submitOrder(message.value.toString(), mongoHelper.db, producer);
+            console.log("Waow received", topic, message);
+            if (topic === "payment_succeeded") {
+                process.exit(0)
+            }
         }
     });
+    let message = {
+        orderId: "waowID123",
+        customer: {},
+        creditCard: {
+            name: "Bob",
+            number: 551512348989,
+            ccv: 775,
+            limit: "07/19"
+        }
+    };
+    await producer.send(
+        {
+            topic: "payment_succeeded", messages: [{key: "", value: message}]
+        })
 };
 
-run().catch(e => console.error(`[example/consumer] ${e.message}`, e));
+run().catch(e => console.error(`[payment_test/consumer] ${e.message}`, e));
 
 const errorTypes = ['unhandledRejection', 'uncaughtException'];
 const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2'];
