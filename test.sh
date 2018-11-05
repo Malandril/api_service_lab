06129b003da24d202f3f5f5ea918c70852372286
Thomas Canava <canava.thomas@gmail.com>
2018-11-05 12:55:02 +0100

    test.sh can now launch only one service with an argument

--- test.sh
+++ test.sh
@@ -8,16 +8,22 @@ function cleanup {
     }
 trap cleanup EXIT
 
-for f in $(ls -d */)
+
+folders=$(ls -d */)
+if [ $# -eq 1 ]; then
+    folders=$1
+fi
+for f in $folders
 do
+    echo $f
     if [ -f $f/test/docker-compose.yml ]; then
         cd "$f/test/"
         echo "===== entering $f ====="
         docker-compose  build
         docker-compose  -f "./docker-compose-test.yml" build
-        docker-compose  up -d
-        sleep 5
-        echo "===== running test ====="
+        docker-compose  up --force-recreate -d 
+        sleep 10
+        echo "===== running test $f ====="
         docker-compose -f "./docker-compose-test.yml" run test --no-deps
         docker-compose down
         cd ../..
