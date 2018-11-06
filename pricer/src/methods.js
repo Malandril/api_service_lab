c7c2d1a748473ee2b92d1573101e87784b0de109
François Melkonian <francois.melkonian@gmail.com>
2018-11-06 21:21:32 +0100

    last try for scenario

--- pricer/src/methods.js
+++ pricer/src/methods.js
@@ -52,11 +52,11 @@ let methods = {
         }
         if (data.voucher) {
             const code = data.voucher;
-            helper.findVoucherByCodeRestaurant(db, restaurantId, code).then(voucher => {
+            findVoucherByCodeRestaurant(db, restaurantId, code).then(voucher => {
                 if (!voucher) {
                     console.log("Voucher " + code + " not found.");
                     value.price = totalPrice;
-                    helper.send_price_computed(producer, value);
+                    send_price_computed(producer, value);
                     return;
                 }
                 else if (voucher.neededCategories) {
@@ -64,20 +64,20 @@ let methods = {
                     let meal_categories = data.meals.map(meal => meal.type.toLowerCase());
                     if (voucher.neededCategories.every(category => meal_categories.includes(category))) {
                         value.price = totalPrice * (1 - voucher.discount);
-                        helper.send_price_computed(producer, value);
+                        send_price_computed(producer, value);
                     } else {
-                        helper.send_price_computed(producer, totalPrice)
+                        send_price_computed(producer, totalPrice)
                     }
                 } else {
                     value.price = totalPrice * (1 - voucher.discount);
-                    helper.send_price_computed(producer, value);
+                    send_price_computed(producer, value);
                 }
             }).catch(reason => {
                 console.log("Error when finding voucher", reason);
             })
         } else {
             value.price = totalPrice;
-            helper.send_price_computed(producer, value);
+            send_price_computed(producer, value);
         }
     }
 };
