b0ad1561ce11b71d75e513171421f2b1dbd41eef
François Melkonian <francois.melkonian@gmail.com>
2018-11-06 20:41:01 +0100

    pls travis

--- pricer/src/helper.js
+++ pricer/src/helper.js
@@ -1,14 +1,12 @@
-module.export = {send_price_computed:send_price_computed , findVoucherByCodeRestaurant:findVoucherByCodeRestaurant};
-
-function send_price_computed(producer, value) {
-    producer.send({
-        topic: "price_computed",
-        messages: [{
-            key: "", value: JSON.stringify(value)
-        }]
-    });
-}
-
-function findVoucherByCodeRestaurant(db, restaurantId, code) {
-    return db.collection("vouchers").findOne({"restaurantId": restaurantId, "code": code});
+module.export = {
+    send_price_computed: function (producer, value) {
+        producer.send({
+            topic: "price_computed",
+            messages: [{
+                key: "", value: JSON.stringify(value)
+            }]
+        });
+    }, findVoucherByCodeRestaurant: function (db, restaurantId, code) {
+        return db.collection("vouchers").findOne({"restaurantId": restaurantId, "code": code});
+    }
 }
